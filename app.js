e7305cb96080d1af01aafce4e094823be0dc94bc
Veronica Tomchak <vtomchak27@gmail.com>
2020-11-19 17:03:08 -0500

    finished up to the install sequelize portion

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const html = require("html-template-tag");
+const layout = require("./views/layout");
+
+
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send(layout(" "));
 });
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in ${PORT}`);
